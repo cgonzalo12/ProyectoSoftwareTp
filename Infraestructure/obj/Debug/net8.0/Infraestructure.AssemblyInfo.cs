a77c9d5898dbb9791aae594281340120db0e1c68
Gonzalo Capararo <cap.gonzalo12@gmail.com>
2024-09-18 21:04:01 -0300

    agregando interfaces proyects

--- Infraestructure/obj/Debug/net8.0/Infraestructure.AssemblyInfo.cs
+++ Infraestructure/obj/Debug/net8.0/Infraestructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b53b3a19267c4203827b8bc0653a4bca872060ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProyectoSofwareTp1/obj/Debug/net8.0/ProyectoSofwareTp1.AssemblyInfo.cs
+++ ProyectoSofwareTp1/obj/Debug/net8.0/ProyectoSofwareTp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoSofwareTp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b53b3a19267c4203827b8bc0653a4bca872060ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoSofwareTp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoSofwareTp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
