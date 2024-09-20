871b465f144e71ad0787cf78072960efa2311c6d
Gonzalo Capararo <cap.gonzalo12@gmail.com>
2024-09-19 21:54:54 -0300

    volviendo a main2

--- Infraestructure/obj/Debug/net8.0/Infraestructure.AssemblyInfo.cs
+++ Infraestructure/obj/Debug/net8.0/Infraestructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc31d65c147e3085f1dd3040cd4529751fa57878")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ab40524ace7360fa1052dab356f6c0d23434aae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
